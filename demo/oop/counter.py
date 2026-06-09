47262c415b0e5910bee212fcc17c8bf36b74a973
KottakotaPavankumar <K.pavanKumar120705@gmail.com>
2026-06-09 13:06:38 +0530

    Add files via upload

---
+++ demo/oop/counter.py
@@ -0,0 +1,27 @@
+class Counter:
+
+    def __init__(self, value = 0):
+        # object attribute
+        self.value = value
+
+    def inc(self, step = 1):
+        self.value += step
+
+    def dec(selfs, step = 1):
+        if  selfs.value == 0:
+            raise ValueError('Counter cannot be negative so decrement is not possible when it is 0')
+        selfs.value -= step
+
+    @property
+    def countervalue(self):
+        return self.value
+
+
+c1 = Counter()  # create an object
+c1.inc(5)
+c1.inc()
+
+print(c1.countervalue)
+
+c2 = Counter(100)
+print(c2.countervalue)
